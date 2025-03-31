6cf61f3df5be33bd81ba955c6eddd3cfa60f4009
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2025-03-31 13:50:54 +0200

    game class skeleton

--- src/main/kotlin/game/Game.kt
+++ src/main/kotlin/game/Game.kt
@@ -3,8 +3,40 @@ package game
 import utility.Vector3
 import kotlin.collections.List
 
-class World(val size: Vector3) {
-    val snake: Snake
-    val obstacles: List<Obstacle>
-    val activePoint: Food
+class Game(val worldSize: Vector3) {
+    /**
+     * Checks whether two GameObjects collide.
+     * @param object1 The first GameObject
+     * @param object2 The second GameObject
+     * @return True if the two GameObjects collide
+     */
+    fun checkCollision(object1: GameObject, object2: GameObject): Boolean {
+
+    }
+
+    /**
+     *  Check if the snake is colliding with anything else.
+     *  @return The GameObject which collides with the snake
+     */
+    fun checkSnakeCollision(): GameObject {
+
+    }
+
+    /**
+     *  Starts the game.
+     */
+    fun startGame() {
+
+    }
+
+    /**
+     *  Ends the game.
+     */
+    fun endGame() {
+
+    }
+
+//    val snake: Snake
+//    val obstacles: List<Obstacle>
+//    var food: Food
 }
