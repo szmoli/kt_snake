b0ef58e14215090aa5c16d6e360f8e6014480770
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2025-03-31 13:37:43 +0200

    snake skeleton

--- src/main/kotlin/game/Snake.kt
+++ src/main/kotlin/game/Snake.kt
@@ -2,7 +2,49 @@ package game
 
 import utility.Vector3
 
-class Snake(var x: Int, var y: Int) : GameObject(x, y) {
-    var direction: Vector3 = Vector3(1, 0, 0)
-    var length: Int = 3
+/**
+ * Constructs a new snake.
+ * @param position The position of the snake in the world.
+ */
+class Snake(position: Vector3) : GameObject(position) {
+    /**
+     * Moves the snake in the current direction.
+     */
+    public fun move() {
+
+    }
+
+    /**
+     * Changes the direction of the snake.
+     * @param newDirection The new direction
+     */
+    public fun changeDirection(newDirection: Vector3) {
+
+    }
+
+    /**
+     * Grows the snake's length by one.
+     */
+    public fun growLength() {
+
+    }
+
+    /**
+     * Resets the snake's speed to 1.
+     */
+    public fun resetSpeed() {
+
+    }
+
+    /**
+     * Sets the snake's speed to 2.
+     */
+    public fun speedUp() {
+
+    }
+
+    private var speed = 1
+    private var length = 1
+    private var direction = Vector3(1, 0, 0)
+    private var body: MutableList<Vector3> = mutableListOf<Vector3>(position)
 }
